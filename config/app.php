1422aeeb4def7bd611ad3293bc9d864c6180a545
David Adokuru <davidadokuru@gmail.com>
2020-09-29 12:11:47 +0100

    updates to user

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
-
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,16 +12,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // Default credentials
-        \App\Models\User::insert([
-            [ 
-                'name' => 'User 1',
+
+        $user1 = User::create([
+            'name' => 'User 1',
+                'email' => 'admin@admin.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'gender' => 'male',
+                'active' => 1,
+                'remember_token' => Str::random(10)
+        ]);
+        $user1->assignRole('Admin');
+
+        $user3 = User::create([
+            'name' => 'User 1',
                 'email' => 'midone@left4code.com',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'gender' => 'male',
                 'active' => 1,
                 'remember_token' => Str::random(10)
-            ]
         ]);
+        $user3->assignRole('User');
+
     }
 }
