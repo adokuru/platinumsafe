0b3c5e0c7df3facb9de2236dda47a8f735a9d5cb
David Adokuru <davidadokuru@gmail.com>
2020-09-30 17:58:40 +0100

    updates

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,7 +17,7 @@ class UsersController extends Controller
     public function index()
     {
         //
-        $users = User::all();
+        $users = ser::role('User')->get();
 
         return view('admin.users.index', compact('users'));
     }
